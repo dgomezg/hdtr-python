2cb628f84662c2dd2cbcdd5caca06b8106c5ec61
David Gomez <dgomezg@gmail.com>
2019-03-17 17:20:04 +0100

    basic pillow merges (copying and pasting) two images

--- pocs/pillow_basic.py
+++ pocs/pillow_basic.py
@@ -8,15 +8,14 @@ SAMPLE_IMAGE_PATH = SOURCE_PATH + "/0001-enf.jpg"
 SAMPLE2_IMAGE_PATH = SOURCE_PATH + "/0384-enf.jpg"
 OUTPUT_PATH = "samples/Toledo/output"
 
+def save_as_tiff(image):
+    output_file_name = OUTPUT_PATH + "/" + datetime.today().strftime('%Y%m%d-%H%M%S%f') + ".tiff"
+    outputImage = image.save(output_file_name)
+    return Image.open(output_file_name)
+
 base_image = Image.open(SAMPLE_IMAGE_PATH)
 print(base_image.format, base_image.size, base_image.size[0], base_image.size[1], base_image.mode)
-
-#base_image.show()
-output_file_name = OUTPUT_PATH + "/" + datetime.today().strftime('%Y%m%d-%H%M%S') + ".tiff"
-outputImage = base_image.save(output_file_name)
-
-savedImage = Image.open(output_file_name)
-
+savedImage = save_as_tiff(base_image)
 
 image2 = Image.open(SAMPLE2_IMAGE_PATH)
 print(image2.format, image2.size, image2.mode)
@@ -24,9 +23,10 @@ print(image2.format, image2.size, image2.mode)
 width = base_image.size[0]
 height = base_image.size[1]
 layer_width_start = int(round(width/2))
+
 layer2 = image2.crop((layer_width_start , 0, width, height))
 savedImage.paste(layer2, (layer_width_start , 0, width, height))
 
 savedImage.show()
-
+save_as_tiff(savedImage)
 
