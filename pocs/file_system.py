2434f8db7939973a57a6ab42806683d487da7992
David Gomez <dgomezg@gmail.com>
2019-03-18 18:23:27 +0100

    pathlib sorted -> Path().glob() does not return files ordered.

--- pocs/file_system.py
+++ pocs/file_system.py
@@ -3,11 +3,13 @@ from pathlib import Path
 #These will be part of the script configuration.
 SOURCE_PATH = "samples/Toledo/sources"
 
-pathlist = Path(SOURCE_PATH).glob('**/*.jpg')
+#Path().glob returns an unsorted list
+pathlist = list([f for f in Path(SOURCE_PATH).glob('**/*.jpg')])
+pathlist.sort()
 for path in pathlist:
     print(str(path))
 
 #Get the number of files in pahtlist
-totalFiles = len([f for f in Path(SOURCE_PATH).glob('**/*.jpg')])
+totalFiles = len(pathlist)
 
 print('there are', totalFiles, 'images to process in', SOURCE_PATH)
