40d090a92946282956080bab24a4b2a944d35827
David Gomez <dgomezg@gmail.com>
2019-04-19 11:23:24 +0200

    More clear var names in generate_mask function. Some tests with Image functions

--- pocs/pillow_basic.py
+++ pocs/pillow_basic.py
@@ -16,13 +16,15 @@ def save_as_tiff(image):
 
 def generate_mask(size):
     mask_value = []
-    for i in range(size[1]):
+    width = size[0]
+    height = size [1]
+    for i in range(height):
         mask_value.append([])
-    for j in range(size[0]):
-        value = 255-(255*j/size[0])
-        for i in range(size[1]):
+    for j in range(width):
+        value = 255-(255*j/width)
+        for i in range(height):
             mask_value[i].append(value)
-    return Image.fromarray(np.array(mask_value))
+    return Image.fromarray(np.array(mask_value), "RGBA")
 
 base_image = Image.open(SAMPLE_IMAGE_PATH)
 print(base_image.format, base_image.size, base_image.size[0], base_image.size[1], base_image.mode)
@@ -31,7 +33,6 @@ savedImage = save_as_tiff(base_image)
 image2 = Image.open(SAMPLE2_IMAGE_PATH)
 print(image2.format, image2.size[0], image2.size[1], image2.mode)
 
-
 width = base_image.size[0]
 height = base_image.size[1]
 layer_width_start = int(round(width/2))
@@ -39,19 +40,25 @@ layer_width_start = int(round(width/2))
 layer2 = image2.crop((0 , 0, width, height))
 print("layer size", layer2.size)
 
-mask_array = np.random.rand(layer2.size[1], layer2.size[0]) > 0.7
+mask_array = np.random.rand(layer2.size[1], layer2.size[0]) 
 print(mask_array)
 mask = Image.fromarray(np.uint8(255*(mask_array)))
 
 #mask = Image.fromarray(np.uint8(255*(np.random.rand(layer2.size[1], layer2.size[0]) > 0.7))) 
 #print("Mask size", mask.size)
-#mask = generate_mask(layer2.size)
-print("mask size", mask.size)
+mask = generate_mask(layer2.size)
+print("mask size", mask.size, " mask mode ", mask.mode)
+
 mask.show()
 
-print("image2 size", image2.size)
+composed_image = Image.composite(savedImage, image2, mask)
+composed_image.show()
+
+print("image2 size", image2.size, "image 2 mode ", image2.mode)
+print("saved image size", savedImage.size, "saved Image mode ", savedImage.mode)
 savedImage.paste(image2, (0, 0, width, height), mask)
 
+image2.show()
 savedImage.show()
 #save_as_tiff(savedImage)
 
